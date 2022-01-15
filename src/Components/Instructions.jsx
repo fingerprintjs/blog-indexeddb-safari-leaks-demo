35b019e5d9ee9ebbfe0ca22484a26631d229d4d3
Martin Bajanik <bayotop@users.noreply.github.com>
2022-01-15 14:49:22 +0100

    fix: Add article and screencast links to the instructions

--- src/Components/Instructions.jsx
+++ src/Components/Instructions.jsx
@@ -11,6 +11,11 @@ export default function Instructions() {
           In Private Safari windows, browse to affected websites within the same tab. After returning to this demo page,
           you will see that your recent browsing history is known to this page via the leak.
         </p>
+        <p>
+          <a href="https://fingerprintjs.com/blog/indexeddb-api-browser-vulnerability-safari-15">Read our article</a> or
+          watch our <a href="https://www.youtube.com/watch?v=Z7dPeGpCl8s">screencast on YouTube</a> for more
+          information.
+        </p>
       </details>
     </section>
   )
